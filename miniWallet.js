b9feaf1a8a72920a7c55e88d16eee8c2537bc05b
roudra323 <asirroudra@gmail.com>
2023-05-29 23:49:47 +0600

    changed etherJS version to 5.x

--- miniWallet.js
+++ miniWallet.js
@@ -2,22 +2,25 @@ const ethers = require("ethers");
 require("dotenv").config();
 
 // Connect to an Ethereum provider (e.g., QUICKNODE)
-const provider = new ethers.JsonRpcProvider(
-  `https://lively-radial-putty.ethereum-sepolia.discover.quiknode.pro/${process.env.QUICKNODE_APIKEY}/}`
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://lively-radial-putty.ethereum-sepolia.discover.quiknode.pro/${process.env.QUICKNODE_APIKEY}/`
 );
 
 // Creates a wallet instance
 const accountPRIV = new ethers.Wallet(process.env.PRIVATE_KEY);
 
 const wallet = accountPRIV.connect(provider);
-const address = wallet.address;
 
 (async () => {
   try {
     console.log(wallet);
 
-    const balance = await provider.getBalance(address);
-    console.log(balance);
+    // gets the balance of the wallet
+    const balance = await wallet.getBalance();
+    const formattedBalance = ethers.utils.formatEther(balance);
+
+    // Prints the readable balance
+    console.log(formattedBalance);
   } catch (error) {
     console.error("Error:", error);
   }
