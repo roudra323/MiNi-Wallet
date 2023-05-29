daee246fbfa86a4b81753c0b73820ea5c718ff1e
roudra323 <asirroudra@gmail.com>
2023-05-30 02:13:52 +0600

    added balance transfer option

--- miniWallet.js
+++ miniWallet.js
@@ -13,14 +13,31 @@ const wallet = accountPRIV.connect(provider);
 
 (async () => {
   try {
-    console.log(wallet);
+    // console.log(wallet);
 
     // gets the balance of the wallet
     const balance = await wallet.getBalance();
     const formattedBalance = ethers.utils.formatEther(balance);
 
     // Prints the readable balance
-    console.log(formattedBalance);
+    console.log(`Account Balance: ${formattedBalance} ether`);
+
+    // Send ETH from the wallet
+    const recipientAddress = process.env.RECEPIENT_ADDRESS;
+    const amountToSend = ethers.utils.parseEther("0.000000000001");
+    const txInfo = await wallet.sendTransaction({
+      to: recipientAddress,
+      value: amountToSend,
+    });
+    console.log("tx Info:\n", txInfo);
+
+    // Wait for the transaction to be confirmed
+    await txInfo.wait();
+
+    // gets the balance of the wallet
+    const balanceAfter = await wallet.getBalance();
+    const formattedBalanceAfter = ethers.utils.formatEther(balanceAfter);
+    console.log(`Account Balance: ${formattedBalanceAfter} ether`);
   } catch (error) {
     console.error("Error:", error);
   }
